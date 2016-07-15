c41ff1ca5f6a0449d57dd42bef10771a410761e1
Joshua Rieken <joshua@joshuarieken.com>
2016-07-14 20:10:01 -0700

    Rename args

--- lib/test_that_json/helpers.ex
+++ lib/test_that_json/helpers.ex
@@ -1,11 +1,12 @@
 defmodule TestThatJson.Helpers do
   import TestThatJson.Parsing
 
-  def parse_json(value) do
-    parse(value)
+  def parse_json(json) do
+    parse(json)
   end
 
-  def parse_json!(value) do
-    parse!(value)
+  def parse_json!(json) do
+    parse!(json)
+  end
   end
 end
