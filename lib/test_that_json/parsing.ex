080e41d81582b59482e4629ba482a4cefccedd41
Joshua Rieken <joshua@joshuarieken.com>
2016-07-15 19:06:24 -0700

    Wrap parsing error

--- lib/test_that_json/parsing.ex
+++ lib/test_that_json/parsing.ex
@@ -7,7 +7,10 @@ defmodule TestThatJson.Parsing do
   def parse!(value), do: value
 
   def parse(json) when is_binary(json) do
-    JSX.decode(json)
+    case JSX.decode(json) do
+      {:error, _} -> {:error, {TestThatJson.InvalidJsonError, [json], "Invalid JSON"}}
+      result      -> result
+    end
   end
   def parse(value), do: {:ok, value}
 end
