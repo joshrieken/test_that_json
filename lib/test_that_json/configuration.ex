b0f5b821727d730f0569901043e467e30eccef4e
Joshua Rieken <joshua@joshuarieken.com>
2016-07-17 21:25:04 -0700

    Add Configuration specs

--- lib/test_that_json/configuration.ex
+++ lib/test_that_json/configuration.ex
@@ -1,7 +1,7 @@
 defmodule TestThatJson.Configuration do
-  @default_excluded_keys ~w(id inserted_at updated_at)
-
   def excluded_keys do
-    Application.get_env(:test_that_json, :excluded_keys, @default_excluded_keys)
+    Application.get_env(:test_that_json, :excluded_keys, default_excluded_keys)
   end
+
+  def default_excluded_keys, do: ~w(id inserted_at updated_at)
 end
---
+++ spec/lib/test_that_json/configuration_spec.exs
@@ -0,0 +1,24 @@
+defmodule TestThatJson.ConfigurationSpec do
+  use ESpec
+
+  alias TestThatJson.Configuration
+
+  describe "excluded_keys" do
+    subject do: Configuration.excluded_keys
+
+    context "with no keys in the environment" do
+      # Assumes no env-configured keys
+      it do: should eq(Configuration.default_excluded_keys)
+    end
+
+    context "with keys in the environment" do
+      let :excluded_keys, do: ~w(some other keys)
+
+      before do
+        allow Application |> to(accept(:get_env, fn(_, _, _) -> excluded_keys end))
+      end
+
+      it do: should eq(excluded_keys)
+    end
+  end
+end
