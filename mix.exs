f49ab1e0409f5288cc39f02296e2e56a6595f27f
Joshua Rieken <joshua@joshuarieken.com>
2016-07-17 20:25:21 -0700

    Relax Elixir version requirement to ~> 1.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TestThatJson.Mixfile do
     [
       app:             :test_that_json,
       version:         "0.1.0",
-      elixir:          "~> 1.3",
+      elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps:            deps(),
