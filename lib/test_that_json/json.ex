4d05918110e9d37648e92eb2950596717e7303b2
Joshua Rieken <joshua@joshuarieken.com>
2016-07-17 21:09:58 -0700

    Allow is_json_equal to not be JSON

--- lib/test_that_json/json.ex
+++ lib/test_that_json/json.ex
@@ -4,43 +4,42 @@ defmodule TestThatJson.Json do
   alias TestThatJson.Pathing
 
   def equals?(subject, value) do
-    with {:ok, processed_subject} <- hard_process(subject),
-         {:ok, processed_value}   <- hard_process(value),
-         do: do_equals?(processed_subject, processed_value)
+    {processed_subject, processed_value} = process(subject, value)
+    do_equals?(processed_subject, processed_value)
   end
 
   def has_keys?(subject, value) do
-    {processed_subject, processed_value} = soft_process(subject, value)
+    {processed_subject, processed_value} = process(subject, value)
     do_has_keys?(processed_subject, processed_value)
   end
 
   def has_only_keys?(subject, value) do
-    {processed_subject, processed_value} = soft_process(subject, value)
+    {processed_subject, processed_value} = process(subject, value)
     do_has_only_keys?(processed_subject, processed_value)
   end
 
   def has_values?(subject, value) do
-    {processed_subject, processed_value} = soft_process_for_values(subject, value)
+    {processed_subject, processed_value} = process_for_values(subject, value)
     do_has_values?(processed_subject, processed_value)
   end
 
   def has_only_values?(subject, value) do
-    {processed_subject, processed_value} = soft_process_for_values(subject, value)
+    {processed_subject, processed_value} = process_for_values(subject, value)
     do_has_only_values?(processed_subject, processed_value)
   end
 
   def has_properties?(subject, value) do
-    {processed_subject, processed_value} = soft_process(subject, value)
+    {processed_subject, processed_value} = process(subject, value)
     do_has_properties?(processed_subject, processed_value)
   end
 
   def has_only_properties?(subject, value) do
-    {processed_subject, processed_value} = soft_process(subject, value)
+    {processed_subject, processed_value} = process(subject, value)
     do_has_only_properties?(processed_subject, processed_value)
   end
 
   def has_path?(subject, path) do
-    processed_subject = soft_process(subject)
+    processed_subject = process(subject)
     do_has_path?(processed_subject, path)
   end
 
@@ -163,26 +162,19 @@ defmodule TestThatJson.Json do
 
 
 
-  defp hard_process(subject) do
-    case hard_parse(subject) do
-      {:ok, parsed_subject} -> {:ok, scrub(parsed_subject)}
-      error                 -> error
-    end
-  end
-
-  defp soft_process(subject) do
-    parsed_subject = soft_parse(subject)
+  defp process(subject) do
+    parsed_subject = parse(subject)
     scrub(parsed_subject)
   end
-  defp soft_process(subject, value) do
-    processed_subject = soft_process(subject)
-    processed_value   = soft_process(value)
+  defp process(subject, value) do
+    processed_subject = process(subject)
+    processed_value   = process(value)
     {processed_subject, processed_value}
   end
 
-  defp soft_process_for_values(subject, value) do
-    parsed_subject = soft_parse(subject)
-    parsed_value   = soft_parse_for_values(value)
+  defp process_for_values(subject, value) do
+    parsed_subject = parse(subject)
+    parsed_value   = parse_for_values(value)
 
     scrubbed_subject = scrub(parsed_subject)
     scrubbed_value   = scrub(parsed_value)
@@ -190,18 +182,14 @@ defmodule TestThatJson.Json do
     {scrubbed_subject, scrubbed_value}
   end
 
-  defp hard_parse(value) do
-    Parsing.parse(value)
-  end
-
-  defp soft_parse(value) do
+  defp parse(value) do
     case Parsing.parse(value) do
       {:ok, parsed_value} -> parsed_value
       {:error, _}         -> value
     end
   end
 
-  defp soft_parse_for_values(value) when is_binary(value) do
+  defp parse_for_values(value) when is_binary(value) do
     case Parsing.parse(value) do
       {:ok, parsed_value} ->
         case parsed_value do
@@ -211,7 +199,7 @@ defmodule TestThatJson.Json do
       {:error, _} -> value
     end
   end
-  defp soft_parse_for_values(value), do: soft_parse(value)
+  defp parse_for_values(value), do: parse(value)
 
   defp scrub(value), do: Exclusion.exclude_keys(value)
 end
--- spec/lib/test_that_json/json/is_json_equal_spec.exs
+++ spec/lib/test_that_json/json/is_json_equal_spec.exs
@@ -40,6 +40,11 @@ defmodule TestThatJson.Assertions.IsJsonEqualSpec do
     end
 
     it do: expect is_json_equal(json, json) |> to(be_true)
+    it do: expect is_json_equal(json, %{
+      "some" => "object",
+      "another" => "key with value",
+      "key_with_object" => %{"nested" => "object y'all"}})
+      |> to(be_true)
     it do: expect is_json_equal(json, slightly_different_json) |> to(be_true)
     it do: expect is_json_equal(json, different_json) |> to(be_false)
   end
@@ -48,35 +53,17 @@ defmodule TestThatJson.Assertions.IsJsonEqualSpec do
     let :json, do: "[1,2,3,4]"
 
     it do: expect is_json_equal(json, json) |> to(be_true)
+    it do: expect is_json_equal(json, [1,2,3,4]) |> to(be_true)
     it do: expect is_json_equal(json, "[1,3,2,4]") |> to(be_false)
     it do: expect is_json_equal(json, "[1,2,3]") |> to(be_false)
     it do: expect is_json_equal(json, "{\"different\": \"json\"}") |> to(be_false)
-
-    context "when the value is not valid JSON" do
-      it "raises an exception" do
-        raiser = fn -> expect is_json_equal(json, "invalid json") |> to(be_true) end
-
-        expect raiser |> to(raise_exception(TestThatJson.InvalidJsonError))
-      end
-    end
   end
 
   context "when the subject is a JSON string" do
     let :json, do: "\"json string\""
 
     it do: expect is_json_equal(json, json) |> to(be_true)
+    it do: expect is_json_equal(json, "json string") |> to(be_true)
     it do: expect is_json_equal(json, "\"josn string\"") |> to(be_false)
-  end
-
-  context "when the subject is not valid JSON" do
-    let :json, do: "invalid"
-
-    context "when the value is not valid JSON" do
-      it "raises an exception" do
-        raiser = fn -> expect is_json_equal(json, "invalid json") |> to(be_true) end
-
-        expect raiser |> to(raise_exception(TestThatJson.InvalidJsonError))
-      end
-    end
   end
 end
