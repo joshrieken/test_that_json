cceba1b967d87efb1b1b276ec74e69fe41e245f9
Joshua Rieken <joshua@joshuarieken.com>
2016-07-17 20:41:32 -0700

    Add assertion tests

---
+++ test/assertions_test.exs
@@ -0,0 +1,89 @@
+defmodule TestThatJson.AssertionsTest do
+  use ExUnit.Case, async: true
+
+  import TestThatJson.Assertions
+
+  test "is_json_equal" do
+    json = "[1,2,3]"
+
+    assert is_json_equal(json, json)
+  end
+
+  test "has_json_keys" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_json_keys(json, ["key1", "key2"])
+  end
+
+  test "has_only_json_keys" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_only_json_keys(json, ["key1", "key2"])
+  end
+
+  test "has_json_values" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_json_values(json, ["value", "data"])
+  end
+
+  test "has_only_json_values" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_only_json_values(json, ["value", "data"])
+  end
+
+  test "has_json_properties" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_json_properties(json, %{"key1" => "value", "key2" => "data"})
+  end
+
+  test "has_only_json_properties" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_only_json_properties(json, %{"key1" => "value", "key2" => "data"})
+  end
+
+  test "has_json_path" do
+    json = """
+    {
+      "key1": "value",
+      "key2": "data"
+    }
+    """
+
+    assert has_json_path(json, "key1")
+  end
+end
+
