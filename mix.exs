48160541c7111b24434f71a8dbfe03675370e1f2
Joshua Rieken <joshua@joshuarieken.com>
2018-05-17 10:14:23 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TestThatJson.Mixfile do
   def project do
     [
       app:             :test_that_json,
-      version:         "0.5.0",
+      version:         "0.6.0",
       elixir:          "~> 1.4",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
