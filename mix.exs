3298a262557ca6fae5572933875bea7bbaa83a17
Joshua Rieken <joshua@joshuarieken.com>
2018-05-17 10:08:53 -0600

    Support elixir >= 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TestThatJson.Mixfile do
     [
       app:             :test_that_json,
       version:         "0.5.0",
-      elixir:          "~> 1.2",
+      elixir:          "~> 1.4",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps:            deps(),
